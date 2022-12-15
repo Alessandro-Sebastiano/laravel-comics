67ca013754363c64674530eec1d8d0e24d010b45
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2022-12-15 19:52:35 +0100

    header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,56 @@
+<header>
+    <div class="bg-primary text-end px-2 text-white">
+        <div class="container-lg">
+            <span class="mx-3">DC POWER VISA</span>
+            <span>ADDITIONAL DC SITES</span>
+        </div>
+    </div>
+    <div class="container bg-white py-3">
+        <div class="d-flex align-items-center justify-content-between">
+            <div class="">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+            </div>
+            <div class="">
+                <ul class="text-uppercase list-unstyled d-flex fw-bold">
+                    <li class="mx-2">
+                        <a href="#">Characters</a>
+                    </li>
+                    <li class="mx-2">
+                        <a href="#">Comics</a>
+                    </li>
+                    <li class="mx-2">
+                        <a href="#">Movies</a>
+                    </li>
+                    <li class="mx-2">
+                        <a href="#">Tv</a>
+                    </li>
+                    <li class="mx-2">
+                        <a href="#">Games</a>
+                    </li>
+                    <li class="mx-2">
+                        <a href="#">Collectibles</a>
+                    </li>
+                    <li class="mx-2">
+                        <a href="#">Videos</a>
+                    </li>
+                    <li class="mx-2">
+                        <a href="#">Fans</a>
+                    </li>
+                    <li class="mx-2">
+                        <a href="#">New</a>
+                    </li>
+                    <li class="mx-2">
+                        <a href="#">Shop</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="border-bottom border-3 border-primary p-1">
+                <input type="text" placeholder="Search" class="text-end border-0 mx-1 text-dark fs-5 input">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+        </div>
+    </div>
+    <div class="jumbo">
+
+    </div>
+</header>
